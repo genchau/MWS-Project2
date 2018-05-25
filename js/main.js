87b50495b1fa0b9cca1680e408df3011c426f8b5
Kathleen Weaver <kathleen@kweaver.org>
2018-05-25 12:38:13 -0500

    Add aria label for view details
    Add aria label for view details

--- js/main.js
+++ js/main.js
@@ -167,6 +167,7 @@ createRestaurantHTML = (restaurant) => {
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
+  more.setAttribute(aria-label, "View Details " + restaurant.name);
   li.append(more)
 
   return li
