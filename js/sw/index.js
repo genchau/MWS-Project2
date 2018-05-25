f724da5ec4691fa2a3bf443d61e284182f3a2969
Kathleen Weaver <kathleen@kweaver.org>
2018-05-25 01:22:25 -0500

    Copied code from WITR Project
    Copied code from WITTR project

---
+++ js/sw/index.js
@@ -0,0 +1,56 @@
+var staticCacheName = 'restreview-static-v1';
+
+/* service worker code is from Udacity course on mobile web design */
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(staticCacheName).then(function(cache) {
+      return cache.addAll([
+        '/skeleton',
+        'js/main.js',
+        'js/dbhelper.js',
+        'js/restaurant_info.js',
+        'css/styles.css',
+        'imgs/icon.png',
+      ]);
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          return cacheName.startsWith('restreview-') &&
+                 cacheName != staticCacheName;
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  var requestUrl = new URL(event.request.url);
+
+  if (requestUrl.origin === location.origin) {
+    if (requestUrl.pathname === '/') {
+      event.respondWith(caches.match('/skeleton'));
+      return;
+    }
+  }
+
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('message', function(event) {
+  if (event.data.action === 'skipWaiting') {
+    self.skipWaiting();
+  }
+});
