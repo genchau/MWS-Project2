5a209c87c0d54a9c1a2a656011b95ed301a314cb
Kathleen Weaver <kathleen@kweaver.org>
2018-05-28 20:59:35 -0500

    Change code to retrieve data properly
    Removed code that retrieved data from internal file to retrieve data from server.

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -20,8 +20,9 @@ class DBHelper {
     xhr.open('GET', DBHelper.DATABASE_URL);
     xhr.onload = () => {
       if (xhr.status === 200) { // Got a success response from server!
-        const json = JSON.parse(xhr.responseText);
-        const restaurants = json.restaurants;
+        /** const json = JSON.parse(xhr.responseText);       Removed code to get data from file
+        const restaurants = json.restaurants;             */
+        const restaurants = JSON.parse(xhr.responseText);
         callback(null, restaurants);
       } else { // Oops!. Got an error from server.
         const error = (`Request failed. Returned status of ${xhr.status}`);
